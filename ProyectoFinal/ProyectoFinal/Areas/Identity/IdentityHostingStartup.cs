ac375a11fbdd09eca293107931e0b96a7b6a6883
nicolasrod008 <54144262+nicolasrod008@users.noreply.github.com>
2019-08-15 11:55:21 -0500

    Initial commit

---
+++ ProyectoFinal/ProyectoFinal/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProyectoFinal.Data;
+
+[assembly: HostingStartup(typeof(ProyectoFinal.Areas.Identity.IdentityHostingStartup))]
+namespace ProyectoFinal.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
